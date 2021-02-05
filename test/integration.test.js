d70e0c50e9d65aba6c57426b00b33439423ceb13
kigcasan <kmeigcasan@gmail.com>
2021-02-06 01:02:29 +0800

    8th attempt

--- test/integration.test.js
+++ test/integration.test.js
@@ -7,36 +7,41 @@ const expect = chai.expect;
 (async function test() {
     try {
         describe ('The Wall - Integration Testing', async function() {
-            let page;
-            this.timeout(50000);
+            // let page;
+            
 
-            beforeEach(async() => {
-                page = new Page();
-            });
+            // beforeEach(async() => {
+            //     page = await new Page();
+            // });
 
-            afterEach(async() => {
-                await page.quit();
-            });
+            // afterEach(async() => {
+            //     await page.quit();
+            // });
 
             it('User is able to login successfully', async() => {
+                let page = await new Page();
                 //---------Perform---------//
                 await page.testSuccessLogin();
                 //---------Expect---------//
                 const result = await page.findById("page_name");
                 const result_text = await page.getText(result);
                 expect(result_text).to.equal("CodingDojo Wall");
+                await page.quit();
             });
 
             it('User is able to register successfully', async() => {
+                let page = await new Page();
                 //---------Perform---------//
                 await page.testSuccessRegister();
                 //---------Expect---------//
                 const result = await page.findById("page_name");
                 const result_text = await page.getText(result);
                 expect(result_text).to.equal("CodingDojo Wall");
+                await page.quit();
             });
 
             it('User is successfully able to log off', async() => {
+                let page = await new Page();
                 //---------Perform---------//
                 await page.testSuccessLogin();
                 await page.testSuccessLogoff();
@@ -44,9 +49,11 @@ const expect = chai.expect;
                 const result = await page.findById("page_name");
                 const result_text = await page.getText(result);
                 expect(result_text).to.equal("Login");
+                await page.quit();
             });
 
             it('User can successfully post a message after logging in', async() => {
+                let page = await new Page();
                 //---------Perform---------//
                 await page.testSuccessLogin();
                 let message = await page.testPostMessage();
@@ -54,15 +61,18 @@ const expect = chai.expect;
                 const result = await page.findByCss("h3~p:last-of-type");
                 const result_text = await page.getText(result);
                 expect(result_text).to.equal(message);
+                await page.quit();
             });
 
             it('User is NOT able to post a message if they are not logged in', async() => {
+                let page = await new Page();
                 //---------Perform---------//
                 await page.testPostMessage_whenUserLogOff();
                 //---------Expect---------//
                 const result = await page.findByCss("pre");
                 const result_text = await page.getText(result);
                 expect(result_text).to.equal("Cannot GET /wall/message");
+                await page.quit();
             });
             
         });
